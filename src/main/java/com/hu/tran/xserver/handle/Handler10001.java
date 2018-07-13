c951587c4acde11a3e7bbfc3e0d0a7ee6f437ceb
hutiantian <hutiantian9527@gmail.com>
2018-07-13 15:42:05 +0800

    进件

--- src/main/java/com/hu/tran/xserver/handle/Handler10001.java
+++ src/main/java/com/hu/tran/xserver/handle/Handler10001.java
@@ -12,14 +12,7 @@ import java.util.Map;
 public class Handler10001 implements Handler{
 
     public void handler(Map<String,Object> request, Map<String,Object> response){
-        response.put("Mac","0000000000001");
-        response.put("TargetSysId","300050");
-        response.put("MsgId","2bc7f1cc-3757-4381-b9c8-5ba787de39aa");
         response.put("ReturnCode","00000000000000");
-        response.put("ReturnMsg","交易成功");
-        response.put("TranDate","20180508");
-        response.put("TranTime","20180508");
-        response.put("GlobalSeqNo","3000501506240286ad09080063e7");
         ArrayList<Map<String,String>> list = new ArrayList();
         Map<String,String> map1 = new HashMap<String, String>();
         map1.put("OtptFile","3000501506240286ad09080063e7_300050.html");
